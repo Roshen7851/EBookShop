8d3584979d93d7baecfd9752e8749bf7fa65dc5b
Roshen7851 <135727343+Roshen7851@users.noreply.github.com>
2024-05-24 11:48:12 +0530

    Delete BookShopManagementSystem directory

--- BookShopManagementSystem/BookShopManagementSystem/Models/Admin.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BookShopManagementSystem.Models
-{
-    public class Admin
-    {
-        public int AdminId { get; set; }
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
